b649109e5659af22e237fef8959be07cf7dbdd9a
rflperry <rflperry@gmail.com>
2022-02-19 17:42:18 +0100

    fix setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 VERSION = 1.0
 PACKAGE_NAME = "honest_forests"
 DESCRIPTION = "Scikit-learn compliant honest decision tree and forest implementations with additional utilities."
-with open("README", "r") as f:
+with open("README.md", "r") as f:
     LONG_DESCRIPTION = f.read()
 AUTHOR = ("Ronan Perry",)
 AUTHOR_EMAIL = "rflperry@gmail.com"
