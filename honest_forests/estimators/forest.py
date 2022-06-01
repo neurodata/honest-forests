929b57a72b288f08ac1854cf0fcc83b2301d4a21
aud <40820553+jzheng17@users.noreply.github.com>
2022-06-01 17:47:33 -0400

    Update forest.py
    fix dependency issues with the new version of joblib (no longer uses **_joblib_parallel_args)

--- honest_forests/estimators/forest.py
+++ honest_forests/estimators/forest.py
@@ -12,7 +12,8 @@ from honest_forests.estimators import HonestTreeClassifier
 from sklearn.utils.validation import check_is_fitted
 from joblib import Parallel, delayed
 from sklearn.ensemble._base import _partition_estimators
-from sklearn.utils.fixes import _joblib_parallel_args
+# new version of joblib doesnt work in the following format
+# from sklearn.utils.fixes import _joblib_parallel_args
 import threading
 
 
@@ -446,7 +447,9 @@ class HonestForestClassifier(ForestClassifier):
         Parallel(
             n_jobs=n_jobs,
             verbose=self.verbose,
-            **_joblib_parallel_args(require="sharedmem")
+            # new version of joblib no longer uses **_joblib_parallel_args
+            # **_joblib_parallel_args(require="sharedmem")
+            require="sharedmem"
         )(
             delayed(_accumulate_prediction)(tree, X, posteriors, lock, idx)
             for tree, idx in zip(self.estimators_, indices)
